513d6e096f87b952d907d7ef2d9c8dd946d71c35
eofoeofo <durk011@naver.com>
2021-01-10 18:26:54 +0900

    2021-01-10 JAVA1 집에서 배열

---
+++ Step1/src/Shstep1/Array1.java
@@ -0,0 +1,16 @@
+package Shstep1;
+
+public class Array1 {
+	public static void main(String[] args) {
+		String[] week = {"월","화","수","목","금","토","일"};
+		System.out.println(week[4]);
+		
+		for(int i=0; i<week.length-1; i++) {
+		
+		for(int j=0; j<week.length; j++) {
+			System.out.print("\n"+week[j]);
+		}
+		System.out.println();
+		}
+	}
+}
---
+++ Step1/src/Shstep1/Array2.java
@@ -0,0 +1,15 @@
+package Shstep1;
+
+public class Array2 {
+	public static void main(String[] args) {
+		int[] ArrNum = new int[9];
+		int rNum = (int)(Math.random() * 10);
+		
+		for(int i=0; i<ArrNum.length-1; i++) {
+			for(int j=0; j<ArrNum.length; j++) {
+				System.out.println(rNum);
+				
+			}
+		}
+	}
+}
